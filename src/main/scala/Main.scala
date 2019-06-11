58e82e2d4aae0cf208480b18971660211589f39c
kroy <kroy@vulog.com>
2019-06-11 16:47:41 +0200

    query implemented

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,29 +6,36 @@ import RunwayData._
 
 object Main extends App {
 
-  // println("Choose Options a or b")
+  println("Choose Options a or b")
+  askUser
+  // for (a <- airportInfo) {
+  //   println(s"Airport List: $a")
+  // }
 
-  // askUser
+  // for (c <- countryInfo) {
+  //   println(s"Country List: $c")
+  // }
 
-  for (a <- airportInfo) {
-    println(s"Airport List: $a")
-  }
-
-  for (c <- countryInfo) {
-    println(s"Country List: $c")
-  }
-
-  for (r <- runwayInfo) {
-    println(s"Runway List: $r")
-  }
+  // for (r <- runwayInfo) {
+  //   println(s"Runway List: $r")
+  // }
 
 }
 
 object askUser {
   val options = Tuple2("Query", "Report")
   val (a, b) = options
-
   val userInput = StdIn.readLine()
-  if (userInput == "a") println(a)
-  else if (userInput == "b") println(b)
+  if (userInput == "a") {
+    println("Enter Country Code; Like: `US` or `AF` ")
+    val country = StdIn.readLine()
+    val airportByCountry = airportInfo.groupBy(_.iso_country.trim).get(country)
+    val airportName = airportByCountry
+    val totalAirportIntheCountry = airportByCountry.get.length
+    println(airportName)
+    println(s"Total number of Airport in $country is $totalAirportIntheCountry")
+  }
+  else {
+    println("Report is coming soon ..... :-)")
+  }
 }
